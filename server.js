67de68908e04f06df04a2bdcc67c60e456e558fe
sandeepraodj <sandeepraodj@gmail.com>
2020-01-01 15:46:04 +0530

    Build Location changes

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ const express = require('express'),
     port = process.env.PORT || 8080,
     app = express();
 
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/build/favicon.ico'));
 
 // the __dirname is the current directory from where the script is running
 
 app.use(express.static(__dirname));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/ping', function (req, res) {
     return res.send('pong');
 });
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port);
